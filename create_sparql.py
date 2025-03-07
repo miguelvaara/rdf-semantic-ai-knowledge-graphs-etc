53f1e26869bdea7e056ea623c0d4f4ebebf66e66
Mika Vaara <mika.vaara@helsinki.fi>
2025-03-07 13:56:53 +0200

    Lisättiin sittenkin uudetSV ja nyt skripti ei lisää sparkkeliin, jos singularPreffi on lähtödatassa tyhjä

--- create_sparql.py
+++ create_sparql.py
@@ -35,6 +35,19 @@ def generate_sparql_update(csv_file):
             # Uniikki uri kaikille
             concept_var = f"?uusi_{index}"
 
+            # concept_triples = [
+            #     f"{concept_var} rdf:type yso-meta:Concept ;",
+            #     f'    skos:prefLabel "{preflabel_fi}"@fi ;',
+            #     f'    skos:prefLabel "{concept_label_en}"@en ;',
+            #     f'    skos:prefLabel "{sv_preflabel}"@sv ;',
+            #     f'    dct:created ?now ;',
+            #     f'    yso-meta:hasThematicGroup yso:p26557 ;',
+            #     f'    yso-meta:singularPrefLabel "{singular_pref_fi}"@fi ;',
+            #     f'    yso-meta:singularPrefLabel "{singular_pref_sv}"@sv ;',
+            #     f'    rdfs:subClassOf yso-update:uudet, yso-update:uudetEn, yso-update:uudetSv, yso:p3749 ;'
+            #     f'    skos:related yso:p19079 ;'
+            # ]
+
             concept_triples = [
                 f"{concept_var} rdf:type yso-meta:Concept ;",
                 f'    skos:prefLabel "{preflabel_fi}"@fi ;',
@@ -42,11 +55,17 @@ def generate_sparql_update(csv_file):
                 f'    skos:prefLabel "{sv_preflabel}"@sv ;',
                 f'    dct:created ?now ;',
                 f'    yso-meta:hasThematicGroup yso:p26557 ;',
-                f'    yso-meta:singularPrefLabel "{singular_pref_fi}"@fi ;',
-                f'    yso-meta:singularPrefLabel "{singular_pref_sv}"@sv ;',
-                f'    rdfs:subClassOf yso-update:uudet, yso-update:uudetEn, yso:p3749 ;'
+                f'    rdfs:subClassOf yso-update:uudet, yso-update:uudetEn, yso-update:uudetSv, yso:p3749 ;',
                 f'    skos:related yso:p19079 ;'
-            ]
+            ] + [
+                f'    yso-meta:singularPrefLabel "{singular_pref_fi}"@fi ;' for _ in [singular_pref_fi] if singular_pref_fi
+            ] + [
+                f'    yso-meta:singularPrefLabel "{singular_pref_sv}"@sv ;' for _ in [singular_pref_sv] if singular_pref_sv
+]
+
+
+
+            
 
             # f'    rdfs:subClassOf yso-update:uudet, yso-update:uudetSv, yso-update:uudetEn, yso-update:uudetSme ;'
 
@@ -62,6 +81,10 @@ def generate_sparql_update(csv_file):
                 concept_triples.append(f'    dct:editorialNote "{note}"@fi ;')
 
             triples.append("\n    ".join(concept_triples) + " .")
+
+            # Seuraava on optio, jota tällä kertaa ei päätetty käyttää            
+            #triples.append("".join(f'yso:p19079 skos:related {concept_var} .'))
+
             binds.append(f"BIND( IRI(CONCAT(str(yso:),'g',struuid())) AS {concept_var} )")
 
     sparql_query = f"""
